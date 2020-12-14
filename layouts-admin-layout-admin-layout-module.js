f0a25b1ecdfda79a8821267fad2c2d12d86624a1
lazydevofficial <lazydevemail@gmail.com>
2020-12-14 12:13:37 -0300

    Update course link

--- layouts-admin-layout-admin-layout-module.js
+++ layouts-admin-layout-admin-layout-module.js
@@ -64856,7 +64856,7 @@ var DashboardComponent = /** @class */ (function () {
                 subtitle: "💻 Curso com Certificado Digital",
                 content: "Durante as aulas você vai entender os principais e mais utilizados recursos da plataforma e de quebra ainda vai construir uma aplicação serverless totalmente do ZERO! ☁️😍",
                 imgUrl: "./assets/img/courses/aws.png",
-                link: "https://www.udemy.com/course/introducao-aws/?couponCode=BB7528F462A764850F3A",
+                link: "https://www.udemy.com/course/introducao-aws/?referralCode=065B3BE466C8131566CC",
                 personal: false,
                 maxWidth: "200px"
             }];
